727a1f526b1c4e8b45ff66b14ff307aeaa196395
chinylv <chinylv@gmail.com>
2014-03-15 00:37:54 +0800

    add Version 1.0 code

---
+++ webzone/js/functions.js
@@ -0,0 +1,49 @@
+function runTimer( inVal ) {
+    aMinute = 60;
+    anHour  = 3600;
+    aDay    = 86400;
+    inval   = parseInt( inVal );
+
+    if ( inVal < 0 || inVal > aDay ) return;
+    var curVal  = inVal;
+    // console.log(curVal);
+
+    hour    = Math.floor( curVal/anHour );
+    curVal  -= curVal >= anHour ? hour * anHour : 0;
+    minute  = Math.floor( curVal/aMinute );
+    curVal  -= curVal >= aMinute ? minute * aMinute : 0;
+
+    var output  = "";
+    if ( hour > 9 ) {
+        output += hour + ":";
+    } else if ( hour > 0) {
+        output += "0" + hour + ":";
+    }
+
+    if ( minute > 9 ) {
+        output += minute + ":";
+    } else {
+        output += "0" + minute + ":";
+    }
+
+    if ( curVal > 9 ) {
+        output += curVal;
+    } else {
+        output += "0" + curVal;
+    }
+
+    if ( inVal == 0 ) output = "Time Up!";
+    // console.log(output);
+    var span = document.getElementById("curTimer").innerText = output;
+}
+
+function startTimer( timeLeft ) {
+    aDay        = 86400;
+    if ( timeLeft == null ) timeLeft = 1500;
+    
+    timeLeft    = parseInt(timeLeft);
+    
+    if ( timeLeft < 0 || timeLeft > aDay) return;
+
+    var si = setInterval( function() { if ( timeLeft >= 0 ) { runTimer(timeLeft); timeLeft--; } }, 1000 );
+}
